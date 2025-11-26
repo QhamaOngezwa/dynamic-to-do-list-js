134e8cedea72335f745a264476b6e5d6ab453a0c
Qhama Hani <qhamahani@gmail.com>
2025-11-26 12:33:51 +0200

    Dom Loaded

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function (){
+    const 
+})
+const userInput = document.getElementById("task-input");
+const addButton = document.getElementById("add-task-button");
