cb3905b70fddb2fa0e3e71dfa5d1bfd66b169630
Qhama Hani <qhamahani@gmail.com>
2025-11-26 13:02:41 +0200

    Add events lidteners

--- script.js
+++ script.js
@@ -10,13 +10,19 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
       const listItem = document.createElement("li");
       listItem.textContent = taskText;
-      const removeButton = document.createElement("remove-btn");
+      const removeButton = document.createElement("button");
       removeButton.textContent = "Remove";
+      removeButton.className = "remove-btn";
       removeButton.addEventListener("click", function () {
         taskList.removeChild(listItem);
       });
+      removeButton.addEventListener("click", function () {
+        taskList.removeChild(listItem);
+      });
+
       listItem.appendChild(removeButton);
       taskList.appendChild(listItem);
+      taskInput.value = "";
     }
   }
   addButton.addEventListener("click", addTask);
